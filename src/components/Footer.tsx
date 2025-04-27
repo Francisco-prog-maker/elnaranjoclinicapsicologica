f3b65c025ff596ffddeda15d32b71f03e5f34e1b
UpBrickIA <upbrickia@gmail.com>
2025-04-27 11:17:30 +0200

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -27,7 +27,7 @@ interface FooterProps {
 
 const Footer = ({
   clinicName = "El Naranjo Clínica Psicológica",
-  phone = "(+56) 981835706",
+  phone = " +56981835706",
   email = "clinicapsicologicaelnaranjo@gmail.com",
   socialLinks = {
     instagram: "https://www.instagram.com/clinicapsicologicaelnaranjo/",
